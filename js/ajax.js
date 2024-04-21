69f113008b419fe03548a33baf7648d689093c61
Weiming Chen <cwm1021@gmail.com>
2024-04-21 14:34:51 +0800

    test....

--- js/ajax.js
+++ js/ajax.js
@@ -7,6 +7,9 @@
                 if (settings.extraData && settings.extraData.indexOf('openai_integration_form') !== -1) {
                     var $conversationWrapper = $('#conversation-wrapper');
                     $conversationWrapper.scrollTop($conversationWrapper[0].scrollHeight);
+
+                    // Reset the input field
+                    $('#openai-integration-form input[name="prompt"]').val('');
                 }
             });
         }
